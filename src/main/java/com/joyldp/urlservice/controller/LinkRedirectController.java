490a3a2fbbbfb9b597527d3ab255ce4e02fc6ad2
Brandon Foster <brandon.foster.vt@gmail.com>
2023-08-29 11:35:44 -0400

    use ui.prefix property

--- src/main/java/com/joyldp/urlservice/controller/LinkRedirectController.java
+++ src/main/java/com/joyldp/urlservice/controller/LinkRedirectController.java
@@ -2,6 +2,7 @@ package com.joyldp.urlservice.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class LinkRedirectController {
 
+    @Value("${ui.prefix}")
+    private String uiPrefix;
     private final UrlService urlService;
 
     public LinkRedirectController(UrlService urlService) {
@@ -23,9 +26,10 @@ public class LinkRedirectController {
 
     @GetMapping("/l/{hash}")
     public String redirect(HttpServletRequest req, @PathVariable(name = "hash") String hash) {
+        log.info("ui.prefix: {}", uiPrefix);
         final UrlEntity urlEntity = urlService.retrieveUrlByHash(hash);
         if (urlEntity == null) {
-            final String notFoundUrl = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + "/m/"
+            final String notFoundUrl = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + uiPrefix + "/m/"
                     + hash;
             log.info("notFoundUrl: {}", notFoundUrl);
             return "redirect:" + notFoundUrl;
